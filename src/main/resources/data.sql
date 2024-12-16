2c819cb16289ae3a4f6fb06226bd917a97c8d479
unknown <maduvivier53@gmail.com>
2024-12-16 11:32:52 -0500

    revert_sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,24 +2,27 @@ drop table if exists message;
 drop table if exists account;
 create table account (
     accountId int primary key auto_increment,
-    username varchar(255) not null unique,
+    username varchar(255) unique,
     password varchar(255)
 );
 create table message (
-    messageId int primary key auto_increment,
-    postedBy int,
-    messageText varchar(255),
-    timePostedEpoch bigint,
-    foreign key (postedBy) references account(accountId)
+    message_id int primary key auto_increment,
+    posted_by int,
+    message_text varchar(255),
+    time_posted_epoch bigint,
+    foreign key (posted_by) references  account(account_id)
 );
 
--- Starting test values with ids of 9999 to avoid test issues
-insert into account values (9999, 'testuser1', 'password');
-insert into account values (9998, 'testuser2', 'password');
-insert into account values (9997, 'testuser3', 'password');
-insert into account values (9996, 'testuser4', 'password');
 
-insert into message values (9999, 9999,'test message 1',1669947792);
-insert into message values (9997, 9997,'test message 2',1669947792);
-insert into message values (9996, 9996,'test message 3',1669947792);
+--add cascade delete/change later
+
+-- Starting test values with ids of 9999 to avoid test issues
+--insert into reimbursement values (9999, 'testuser1', 'password');
+--insert into reimbursement values (9998, 'testuser2', 'password');
+--insert into reimbursement values (9997, 'testuser3', 'password');
+--insert into reimbursement values (9996, 'testuser4', 'password');
+
+--insert into user values (9999, 9999,'test user 1',1669947792);
+--insert into user values (9997, 9997,'test user 2',1669947792);
+--insert into user values (9996, 9996,'test user 3',1669947792);
 
